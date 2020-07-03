93bb01cf72ee23a0458ec612384961e92210fb2c
root2raj <ruturaj1999@gmail.com>
2020-07-03 07:46:16 +0530

    msm8953-common: Use vince specific kernel

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -49,9 +49,7 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-ifneq ($(TARGET_KERNEL_SOURCE),)
-    TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8953
-endif
+TARGET_KERNEL_SOURCE := kernel/xiaomi/vince
 TARGET_KERNEL_VERSION := 4.9
 
 # ANT
